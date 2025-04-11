b2b60f7c0fc9e31e55d0ef6c4c5b152c34fa9bfa
Egor Dmitriev <dmpdl@MacBook-Pro-Egor.local>
2025-04-11 18:47:59 +0300

    config to separate folder

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,21 +3,12 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
-	"net"
+	"kvdb/cmd/server/config"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-
-	"kvdb/internal/compute"
-	serverConfig "kvdb/internal/config/server"
-	"kvdb/internal/database"
-	"kvdb/internal/network/server"
-	"kvdb/internal/rpc/query"
-	"kvdb/internal/storage/inmemory"
 )
 
 func main() {
@@ -26,19 +17,19 @@ func main() {
 
 	mainLogger := zap.NewExample()
 
-	config, err := loadConfig(*configPath)
+	conf, err := config.LoadConfig(*configPath)
 	if err != nil {
 		mainLogger.Fatal("failed load config", zap.Error(err))
 	}
 
-	logger, err := initLogger(config)
+	logger, err := config.InitLogger(conf)
 	if err != nil {
 		mainLogger.Fatal("failed init logger", zap.Error(err))
 	}
 
-	db := initDatabase(logger)
+	db := config.InitDatabase(logger)
 
-	tcpServer, err := initServer(config, logger, db)
+	tcpServer, err := config.InitServer(conf, logger, db)
 	if err != nil {
 		mainLogger.Fatal("failed init server", zap.Error(err))
 	}
@@ -55,68 +46,4 @@ func main() {
 	}()
 
 	tcpServer.Listen(ctx)
-}
-
-func loadConfig(configPath string) (*serverConfig.Config, error) {
-	f, err := os.Open(configPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed read file: %s: %w", configPath, err)
-	}
-	defer f.Close()
-
-	return serverConfig.LoadConfig(f)
-}
-
-func initLogger(config *serverConfig.Config) (*zap.Logger, error) {
-	var level zapcore.Level
-	err := level.UnmarshalText([]byte(config.Logging.Level))
-	if err != nil {
-		return nil, fmt.Errorf("unexpected log level: %w", err)
-	}
-
-	var output zapcore.WriteSyncer
-	if config.Logging.Output == "" || config.Logging.Output == "stdout" {
-		output = os.Stdout
-	} else {
-		file, err := os.OpenFile(config.Logging.Output, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			return nil, fmt.Errorf("failed open log file: %w", err)
-		}
-		output = zapcore.AddSync(file)
-	}
-
-	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-
-	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(encoderConfig),
-		output,
-		level,
-	)
-
-	logger := zap.New(core)
-	return logger, nil
-}
-
-func initDatabase(logger *zap.Logger) *database.Database {
-	compute := compute.New()
-	storage := inmemory.New()
-	return database.New(logger, compute, storage)
-}
-
-func initServer(conf *serverConfig.Config, logger *zap.Logger, db *database.Database) (*server.TCPServer, error) {
-	listener, err := net.Listen("tcp", conf.Network.Address)
-	if err != nil {
-		return nil, err
-	}
-
-	queryHandler := query.New(db, logger)
-
-	tcpServer := server.New(logger, listener).
-		WithMaxConn(conf.Network.MaxConnections).
-		WithMaxMessageSize(conf.Network.MaxMessageSizeBytes).
-		WithIdleTimeout(conf.Network.IdleTimeout).
-		WithQueryHandleFunc(queryHandler.Handle)
-
-	return tcpServer, nil
 }
